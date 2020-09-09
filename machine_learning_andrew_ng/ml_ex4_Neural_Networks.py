a1b794a58201c470ce2b774f791ede5dbb13b98f
Nexus Gong <you@nexus_me@hotmail.com>
2020-09-09 17:42:49 +0800

    Add annotations

--- machine_learning_andrew_ng/ml_ex4_Neural_Networks.py
+++ machine_learning_andrew_ng/ml_ex4_Neural_Networks.py
@@ -31,6 +31,7 @@ def load_data(path):
     return X, y
 
 
+# show 100 images
 def plot_100_images(X_in, y_in):
     random_100 = np.random.permutation(X_in.shape[0])[:100]  # choose 100 rows
     images = X_in[random_100, :]  # (100,400)
@@ -54,12 +55,13 @@ def sigmoid_gradient(z):
     return np.multiply(sigmoid(z), (1 - sigmoid(z)))
 
 
+# 前馈
 def forward_propagate(m_in, X, theta1, theta2):
-    a1 = np.insert(X, 0, values=np.ones(m_in), axis=1)
+    a1 = np.insert(X, 0, values=np.ones(m_in), axis=1)  # X0假设为1，为求出weights的第一个参数，加了一列1
     z2 = a1 @ theta1.T
-    a2 = np.insert(sigmoid(z2), 0, values=np.ones(m_in), axis=1)
+    a2 = np.insert(sigmoid(z2), 0, values=np.ones(m_in), axis=1)  # 求a2
     z3 = a2 @ theta2.T
-    h = sigmoid(z3)
+    h = sigmoid(z3)  # hθx 结果矩阵
     return a1, z2, a2, z3, h
 
 
@@ -73,15 +75,15 @@ def encode_y(y_in):
 def nnCostFunction(nn_params, input_size, hidden_size, label_num, X_in, y_in, lambda_in):
     m = len(X_in)
 
-    y_fi = encode_y(y_in)
+    y_fi = encode_y(y_in)  # 对y标签进行编码 需要转为5000*10
 
-    theta_1 = nn_params[:hidden_size * (input_size + 1)].reshape(hidden_size, input_size + 1)
+    theta_1 = nn_params[:hidden_size * (input_size + 1)].reshape(hidden_size, input_size + 1)  # 提取theta1 25 * 401
 
-    theta_2 = nn_params[hidden_size * (input_size + 1):].reshape(num_labels, hidden_size + 1)
+    theta_2 = nn_params[hidden_size * (input_size + 1):].reshape(num_labels, hidden_size + 1)  # 提取theta2 10 * 26
 
-    a_1, z_2, a_2, z_3, h = forward_propagate(m, X_in, theta_1, theta_2)
+    a_1, z_2, a_2, z_3, h = forward_propagate(m, X_in, theta_1, theta_2)  # 使用前馈算法
 
-    # compute the cost
+    # compute the cost 根据公式求出cost
     J = 0
     for i in range(m):
         first_term = -y_fi[i, :] @ np.log(h[i, :])
@@ -91,7 +93,7 @@ def nnCostFunction(nn_params, input_size, hidden_size, label_num, X_in, y_in, la
     return J
 
 
-# 正则化
+# 正则化cost function
 def nnCostFunctionReg(nn_params, input_size, hidden_size, label_num, X_in, y_in, lambda_in):
     m = len(X_in)
 
@@ -111,12 +113,12 @@ def nnCostFunctionReg(nn_params, input_size, hidden_size, label_num, X_in, y_in,
         J += np.sum(first_term - second_term)
     J = J / m
 
-    # add the cost regularization term
+    # add the cost regularization term 正则化项
     reg = (float(lambda_in) / (2 * m)) * (np.sum(np.power(theta_1[:, 1:], 2)) + np.sum(np.power(theta_2[:, 1:], 2)))
     return J + reg
 
 
-# 随机初始theta
+# 随机初始theta 打破对称性
 def rand_ini_para(hidden_size_in, input_size_in):
     # np.random.random(size) 返回size大小的0-1随机浮点数
     params = (np.random.random(
@@ -124,7 +126,7 @@ def rand_ini_para(hidden_size_in, input_size_in):
     return params
 
 
-# 反向传播
+# 反向传播 返回正确的梯度下降结果
 def back_prop(nn_params, input_size, hidden_size, label_num, X_in, y_in, lambda_in):
     m = len(X_in)
 
@@ -134,6 +136,7 @@ def back_prop(nn_params, input_size, hidden_size, label_num, X_in, y_in, lambda_
 
     theta_2 = nn_params[hidden_size * (input_size + 1):].reshape(num_labels, hidden_size + 1)
 
+    # “前向传递”计算整个神经网络的所有激活元，包括假设hΘ（x）的输出值
     a_1, z_2, a_2, z_3, h = forward_propagate(m, X_in, theta_1, theta_2)
 
     # compute the cost
@@ -147,21 +150,24 @@ def back_prop(nn_params, input_size, hidden_size, label_num, X_in, y_in, lambda_
         J += np.sum(first_term - second_term)
     J = J / m
 
-    # back prop
+    # back prop 最小化J(Θ)
+    # 对于层l中的每个节点j，我们要计算一个“误差项”δ（l)j，它测量出该节点对输出中的任何错误“负责”j的程度
     for t in range(m):
-        # a_1 = np.mat(a_1)
+        # Set the input layer’s values (a(1) ) to the t-th training example x(t)
         a1t = np.mat(a_1[t, :])  # (1, 401)
-        z2t = np.mat(z_2[t, :])  # (1, 25)
-        a2t = np.mat(a_2[t, :])  # (1, 26)
-        ht = np.mat(h[t, :])  # (1, 10)
-        yt = np.mat(y_fi[t, :])  # (1, 10)
+        z2t = np.mat(z_2[t, :])  # (1, 25) computing the activations z(2)
+        a2t = np.mat(a_2[t, :])  # (1, 26) a(2)
+        ht = np.mat(h[t, :])  # (1, 10) z(3)
+        yt = np.mat(y_fi[t, :])  # (1, 10) original
 
-        d3t = ht - yt  # (1, 10)
+        d3t = ht - yt  # (1, 10) 算出error
+
+        # 计算δ(2)
         z2t = np.insert(z2t, 0, values=np.ones(1))  # (1, 26)
-
         gz = sigmoid_gradient(z2t)
         d2t = np.multiply((theta_2.T * d3t.T).T, gz)  # (1, 26)
 
+        #  删除 δ(2)0
         delta1 = delta1 + (d2t[:, 1:]).T * a1t
         delta2 = delta2 + d3t.T * a2t
 
@@ -213,6 +219,7 @@ def back_prop_reg(nn_params, input_size, hidden_size, label_num, X_in, y_in, lam
 
     delta1 = delta1 / m
     delta2 = delta2 / m
+
     # add the gradient regularization term
     delta1[:, 1:] = delta1[:, 1:] + (theta_1[:, 1:] * lambda_in) / m
     delta2[:, 1:] = delta2[:, 1:] + (theta_2[:, 1:] * lambda_in) / m
