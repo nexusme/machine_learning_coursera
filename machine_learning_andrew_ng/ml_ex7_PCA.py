12f4effdd87d2d8715d139efd2d7d23b1f199724
Nexus Gong <you@nexus_me@hotmail.com>
2020-09-23 14:48:50 +0800

    Rename files

---
+++ machine_learning_andrew_ng/ml_ex7_PCA.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import scipy
+import sklearn
+from skimage import io
+from sklearn import svm
+import numpy as np
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+import random
+
+path1 = 'ex7data1.mat'
