f14a3c188d28977460c1e7943bec958399b8b07b
Nexus Gong <you@nexus_me@hotmail.com>
2020-09-23 14:43:24 +0800

    finish ex7

--- machine_learning_andrew_ng/ml_ex7_KC_and_PCA.py
+++ machine_learning_andrew_ng/ml_ex7_KC_and_PCA.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import scipy
 import sklearn
@@ -20,36 +19,47 @@ def read_data(path):
     return read_X
 
 
-def read_image(path):
+# Part 4: K-Means Clustering on Pixels
+
+def on_pixles(path):
     data = io.imread(path)
-    print(data)
+    # print('data:', data.shape)
+    mm, nn, kk = data.shape
+    print(mm, nn, kk)
+    data = data / 255
+    X = np.reshape(data, [mm * nn, 3])
+    return data, X
 
 
 def find_closest_centroids(x_in, cent):
-    idx = np.zeros([len(x_in), 1])
-    err_list = []
-    for i in range(len(x_in)):
-        one_row = np.mat([[x_in[i][0], x_in[i][1]], [x_in[i][0], x_in[i][1]], [x_in[i][0], x_in[i][1]]])
-        err_list.append([np.sum(row[0]) for row in np.power(one_row - cent, 2)])
-    for i in range(len(err_list)):
-        idx[i] = err_list[i].index(min(err_list[i])) + 1
+    m, n = x_in.shape
+    # 形心数量
+    centroids = np.reshape(cent, [len(cent), n])
+    K = centroids.shape[0]
+    idx = np.zeros([m, 1])
+    for i in range(0, m):
+        distance_t = np.sum(np.power((x_in[i, :] - centroids[0, :]), 2))
+        j_t = 0
+        for j in range(1, K):
+            distance = np.sum(np.power((x_in[i, :] - centroids[j, :]), 2))
+            if distance < distance_t:
+                distance_t = distance
+                j_t = j
+        idx[i] = j_t
     return idx
 
 
-# Compute means based on the closest centroids found in the previous part.
 def compute_centroids(X_in, idx_in, K_in):
-    list_pre = [[idx_in.tolist()[i][0], X_in[i][0], X_in[i][1]] for i in range(len(X_in))]
-    # centroids = np.mat([cc_df(list_pre, i) for i in range(1, k + 1)])
-    # print(centroids)
-    return np.mat([cc_df(list_pre, i) for i in range(1, K_in + 1)])
-
-
-def cc_df(list_in, type_value):
-    df = pd.DataFrame(list_in, columns=['type', 'x1', 'x2'])
-    df_type = df[df['type'] == type_value].sum()
-    value = df_type['type'] / type_value
-    df_type = df_type / value
-    return [df_type['x1'], df_type['x2']]
+    mm, nn = X_in.shape
+    centroids = np.zeros([K_in, nn])
+    for ii in range(0, K_in):
+        count = 0
+        for j in range(0, mm):
+            if idx_in[j] == ii:
+                centroids[ii, :] += X_in[j, :]
+                count += 1
+        centroids[ii, :] /= count
+    return centroids
 
 
 def plot_data_points(X, idx_in, K):
@@ -65,10 +75,20 @@ def plot_progress_k_means(X, centroids_in, idx_in, K):
         plt.plot(centroids_in[:, 0 + 2 * i], centroids_in[:, 1 + 2 * i], 'kx--', markersize=8)
 
 
+def k_means_init_centroids(X_in, K_in):
+    # cent = np.zeros([K_in, len(X_in)])
+    index_list = [i for i in range(len(X_in))]
+    random.shuffle(index_list)  # Randomly reorder the indices of examples
+    # print(index_list)
+    cent = X_in[index_list[0:K_in], :]  # Take the first K examples as centroids
+    # print(cent)
+    return cent
+
+
 def run_k_means(X, initial_centroids, max_iters, plot_progress):
-    m, n = X.shape
+    m, n = X.shape  # n is 3
     id_x = np.zeros([m, 1])
-    K = len(initial_centroids)
+    K = len(initial_centroids)  # k is 3
     initial_centroids = np.reshape(initial_centroids, [K, n])
     centroid = initial_centroids
     # 将每次迭代的形心点放在一行
@@ -81,7 +101,6 @@ def run_k_means(X, initial_centroids, max_iters, plot_progress):
         id_x = find_closest_centroids(X, centroid)
         centroid = compute_centroids(X, id_x, K)
         centroids_history = np.row_stack([centroids_history, centroid.flatten()])
-
     if plot_progress:
         plot_progress_k_means(X, centroids_history, id_x, K)
     plt.show()
@@ -95,7 +114,26 @@ if __name__ == '__main__':
     raw_data = read_data(path1)
 
     # Find the closest centroids for the examples using the initial_centroids
-    # idx_value = find_closest_centroids(raw_data, ini_centroids) # test
-    # centroids_out = compute_centroids(raw_data, idx_value, k) # test
-    # [centroid_final, idx_final] = run_k_means(raw_data, ini_centroids, max_iter, True)  # K-means on example dataset
-    read_image(path2)
+    # idx_value = find_closest_centroids(raw_data, ini_centroids)  # test
+    # centroids_out = compute_centroids(raw_data, idx_value, k)  # test
+    # [centroid_final, idx_final] = run_k_means(raw_data, ini_centroids, max_iter, True)  # K-means on example data set
+
+    og_data, raw_data_2 = on_pixles(path2)
+    ini_cent_2 = k_means_init_centroids(raw_data_2, k)
+    [centroid_final, idx_final] = run_k_means(raw_data_2, ini_cent_2, max_iter, False)  # K-means on example data set
+
+    # Image Compression
+    idx_f = find_closest_centroids(raw_data_2, centroid_final)
+    X_recovered = np.zeros_like(raw_data_2)
+    m1, n1, K1 = 128, 128, 3
+    print(m1, n1, K1)
+    for i in range(0, m1 * n1):
+        X_recovered[i] = centroid_final[int(idx_f[i, 0])]
+    X_recovered = X_recovered.reshape([m1, n1, K1])
+
+    fig, axes = plt.subplots(nrows=1, ncols=2)
+    axes[0].imshow(og_data)
+    axes[0].set_title('original')
+    axes[1].imshow(X_recovered)
+    axes[1].set_title('Compressed,' + str(K1) + ' colors.')
+    plt.show()
